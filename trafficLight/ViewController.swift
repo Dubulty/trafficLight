5547473ff0910a9e376a207284a5c50be6caf495
Dubulty <118088466+Dubulty@users.noreply.github.com>
2024-02-08 15:26:27 +0400

    first commit

---
+++ trafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLight
+//
+//  Created by Андрей on 08.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
